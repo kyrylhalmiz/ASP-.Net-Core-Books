bd43e067649316a6b212b34c2143bd0c668fb99f
kyrylhalmiz@gmail.com <kyrylhalmiz@gmail.com>
2020-07-07 19:08:46 +0300

    Added Constrains to data and changed "Welcome" and "Privacy policy" pages

--- MvcBooks/Models/Book.cs
+++ MvcBooks/Models/Book.cs
@@ -10,12 +10,16 @@ namespace MvcBooks.Models
     public class Book
     {
         public int Id { get; set; }
+        [StringLength(50, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+        [StringLength(50, MinimumLength = 2)]
         public string Author { get; set; }
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(100, 2)")]
         public decimal Price { get; set; }
     }
